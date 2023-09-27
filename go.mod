4b6c6c0527a447ee0a4433faba53af770333731d
JoseHurtadoBaeza <josehurtadobaeza@gmail.com>
2023-09-27 05:32:04 +0200

    Created the main code, now let's create the handler functions

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/JoseHurtadoBaeza/Movies-CRUD-API-Golang-freeCodeCamp
+
+go 1.21.1
+
+require github.com/gorilla/mux v1.8.0 // indirect
---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+}
+
+var movies []Movie
+
+func main() {
+
+	r := mux.NewRouter()
+
+	movies = append(movies, Movie{ID: "1", Isbn: "438227", Title: "Indiana Jones", Director: &Director{FirstName: "Steven", LastName: "Spielberg"}})
+	movies = append(movies, Movie{ID: "2", Isbn: "45455", Title: "Terminator", Director: &Director{FirstName: "James", LastName: "Cameron"}})
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Printf("Starting server at port 8080\n")
+	log.Fatal(http.ListenAndServe(":8080", r))
+
+}
